0ceed2ba025b73bc103e42d682a0172d17d4ebdc
Daniel Sung <DanielChanSung@gmail.com>
2021-01-11 13:54:02 -0500

    Done.

--- lib/method arguments.rb
+++
@@ -1,9 +0,0 @@
-# lib/introduction.rb
-
-def introduction(Daniel)
-    puts "Hi, my name is #{name}."
-end
-
-def introduction_with_language (Daniel,Ruby)
-    puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
